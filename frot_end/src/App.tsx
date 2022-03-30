e5525f282e2f2edcebd767935d0ba9a406a2eb44
Raniel <raniel.paula@fatec.sp.gov.br>
2022-03-30 13:28:08 -0300

    Upload de paginas: Admin,Pri.Acesso e Uploads

--- frot_end/src/App.tsx
+++ frot_end/src/App.tsx
@@ -3,13 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import DadosPessoais from './components/dados_pessoais/dadosPessoais';
+import Administrador from './components/Administrador/Administrador';
+import PrimeiroAcesso from './components/PrimeiroAcesso/PrimeiroAcesso';
+import Upload from './components/Upload/Upload';
 
 
 function App() {
   return(
     <div>
       <NavBar/>
-      <DadosPessoais/>
+      <Upload />
       
     </div>
   )
