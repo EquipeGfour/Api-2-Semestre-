88695682b58ebc7564a02557a21083e2c965280a
Rodrigo Ribeiro <rodrigo.rsantos40@gmail.com>
2022-04-02 19:20:13 -0300

    gerador de senha aleatoria e envio de email.

--- backend/controllers/preRegistro.js
+++ backend/controllers/preRegistro.js
@@ -1,14 +1,55 @@
 import Colaborador from "../models/colaborador.js";
+import NodeMailer  from "nodemailer"
+
+
+const transporter = NodeMailer.createTransport({ 
+    service: 'gmail', 
+    auth: { 
+       user: 'gfourtestes@gmail.com ', 
+       pass: 'testes321' 
+     } 
+    });
 
 export const insertPreRegistro = async(req, res) => {
     try{
+        const senha = geradorSenha()
+        const email = req.body.email
         const dados = await Colaborador.create({
             email:req.body.email ,
-            senha:req.body.senha
-        })
+            senha:senha
+        });
+
+
+        const mailOptions = {
+            from: 'gfourtestes@gmail.com', // sender address
+            to: `${email} `, // receiver (use array of string for a list)
+            subject: 'Subject of your email', // Subject line
+            html:`<p>seu token para login<p> <strong> ${senha} </strong>`
+    };
+    
+    transporter.sendMail(mailOptions, (err, info) => {
+        if(err)
+          console.log(err)
+        else
+          console.log(info);
+     });
+    
+
         return res.json('Registro Inserido')
         
     }catch (error) {
         res.json({ message: error.message });
     }
 }
+
+function geradorSenha (){
+    const rand=()=>Math.random(0).toString(36).substr(2);
+    const token=(length)=>(rand()+rand()+rand()+rand()).substr(0,length);
+    return token(12)
+}
+
+
+
+
+
+
