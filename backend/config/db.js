2dac47aff611daeab36369fedae097a8defa0b4b
Rodrigo Ribeiro <rodrigo.rsantos40@gmail.com>
2022-04-05 15:42:45 -0300

    atualização .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
+import 'dotenv/config' 
 
-const db = new Sequelize('ionic','root','senha' ,{
+const db = new Sequelize(process.env.BD,'root',process.env.BD_SENHA,{
     host:'localhost',
     dialect:'mysql',
     port:'3306'
--- backend/config/email.js
+++ backend/config/email.js
@@ -3,7 +3,7 @@ import NodeMailer  from "nodemailer"
 export const transporter = NodeMailer.createTransport({ 
     service: 'gmail', 
     auth: { 
-       user: 'gfourtestes@gmail.com', 
-       pass: 'senha321' //colocar senha do email 
+       user: process.env.EMAIL, 
+       pass: process.env.EMAIL_SENHA //colocar senha do email 
      } 
     });
