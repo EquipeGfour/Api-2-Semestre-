c6458aa1431bc8023b1ff2ed4de909d2c6698475
Rodrigo Ribeiro <rodrigo.rsantos40@gmail.com>
2022-04-07 15:45:34 -0300

    alterado query validação login.

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,29 +1,31 @@
 import Colaborador from "../models/colaborador.js"
 import Cargos from "../models/cargo.js"
 import pessoafisica from "../models/pessoafisica.js"
+import sequelize from '../config/db.js'
 
 export const verify = async (req, res) => {
     try{
-        const dados = await Colaborador.findOne({ where: { email: req.body.email, senha:req.body.senha},
-        include:[Cargos]
+        const email = req.body.email
+        const senha = req.body.senha
+
+        const dados = await sequelize.query(`
+            select c.email,c.senha,c.ID, c.nome, pf.cpf, pj.cnpj
+                from colaboradors c left join pessoa_fisicas as pf 
+                    on c.ID = pf.colaboradorId 
+                left join pessoa_juridicas as pj 
+                    on c.ID = pj.colaboradorId
+                where c.email = '${email}' and c.senha = '${senha}'`,
+            {type:sequelize.QueryTypes.SELECT})
         
-        })
-        // res.json(dados)
         if(dados === null ){
             res.status(401).json({"message":"Email ou Senha incorretos"})
         }
         else{
-            const cpf = await findIdPessoaFisica(dados)
-            res.json({"message":"Login Realizado com Sucesso",dados,cpf}) 
+            res.json({"message":"Login Realizado com Sucesso",dados}) 
         } 
     } catch (error) {
         res.json({ message: error.message });
     } 
 }
 
-export const findIdPessoaFisica = async (pessoa) =>{
-    const cpf = await pessoafisica.findOne({where:{
-        colaboradorid:pessoa.id
-    }})
-    return cpf
-}
+
