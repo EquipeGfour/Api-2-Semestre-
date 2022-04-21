b9a253c8d0447fbe1d4a697eaa4a2f0080e03330
Rodrigo Ribeiro <rodrigo.rsantos40@gmail.com>
2022-04-21 17:25:45 -0300

    ajustado rota de Get cargo

--- backend/routes/cargoRouter.js
+++ backend/routes/cargoRouter.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { getCargosDepartamentos } from "../controllers/cargoController.js";
 import { getCargoColaborador } from "../controllers/colaborador.js";
 import { inserirCargo } from "../controllers/insertCargo.js";
 import { verifyJWT } from "../controllers/login.js";
@@ -7,5 +8,5 @@ import { verifyJWT } from "../controllers/login.js";
 const router = Router()
 
 router.post('/insertCargo',verifyJWT,inserirCargo)
-router.get('/getCargo/:id',verifyJWT,getCargoColaborador)
+router.get('/getCargo/:id',verifyJWT,getCargosDepartamentos)
 export default router
