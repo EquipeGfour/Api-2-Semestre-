8ba291e64fc19e8e75c0e5a2ebe95ec772b44b94
Nicolas Lima <nichollaslimma734@gmail.com>
2022-04-26 20:02:08 -0300

    Criado Upload de arquivos

--- backend/routes/colaboradorRouter.js
+++ backend/routes/colaboradorRouter.js
@@ -1,8 +1,23 @@
 import { Router }  from "express";
 import { getAllColaborador, testeCargo, testePessoaFisica,inserirDadosColab, geralFunc,inserirDadosColabCnpj, getColaboradorById} from "../controllers/colaborador.js";
 
-import {verifyJWT} from "../controllers/login.js"
+import {verifyJWT} from "../controllers/login.js";
+import multer from 'multer';
+import path from 'path';
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, "./uploads");
+    },
+    filename: (req, file, cb) => {
+        const { name, ext } = path.parse(file.originalname);
+
+        cb(null, `${name}.${ext}`);
+    }
+});
+
+const upload = multer({ storage })
+const diretorio = multer({dest:'uploads/'})
 const router = Router()
 
 router.get('/', verifyJWT, getAllColaborador)
@@ -12,5 +27,11 @@ router.post('/novo', verifyJWT, inserirDadosColab)
 router.get('/geral', verifyJWT,geralFunc)
 router.post('/cnpj', verifyJWT, inserirDadosColabCnpj)
 router.get('/funcionario/:id',verifyJWT, getColaboradorById)
+
+
+router.post('/upload', upload.array("arquivo",8), (req,res) => {
+    const dados = req.body;
+    res.json(dados);
+});
 
 export default router
