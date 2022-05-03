15bc431ca2bd28c9b36dcea21beb11b7e7ff589f
Nicolas Lima <nichollaslimma734@gmail.com>
2022-05-03 16:00:58 -0300

    criado end point para seletor

--- backend/controllers/preRegistroController.js
+++ backend/controllers/preRegistroController.js
@@ -2,7 +2,8 @@ import { sendMail } from "../functions/sendEmail.js";
 import { geradorSenha } from "../functions/gerador_senha.js";
 import { createPessoaFisica } from "../service/pessoaFisicaService.js";
 import { createPessoaJuridica } from "../service/pessoJuridicaService.js";
-
+import Departamento from "../models/departamentos.js";
+import Cargos from "../models/cargo.js"
 
 export const insertPreRegistroCpf = async(req, res) => {
     try{
@@ -13,6 +14,7 @@ export const insertPreRegistroCpf = async(req, res) => {
             Colaborador:{
                 nome:req.body.nome,
                 email:req.body.email,
+                Cargos_ID:req.body.Cargos_ID,
                 senha:senha,
             }
         }
@@ -35,6 +37,7 @@ export const insertPreRegistroCnpj = async(req, res) => {
             Colaborador:{
                 nome:req.body.nome,
                 email:req.body.email,
+                Cargos_ID:req.body.Cargos_ID,
                 senha:senha,
             }
         }
@@ -44,6 +47,21 @@ export const insertPreRegistroCnpj = async(req, res) => {
 
     }catch (error) {
         res.status(500).json({ message: error.message });
+    }
+}
+
+export const getDepartCargo =  async (req, res) => {
+    try{
+        const dados = await Departamento.findAll({
+            attributes:['id','area'],
+            include:{
+                model:Cargos,
+                attributes:['ID', 'cargo', 'Departamento_ID']
+            }
+        })
+        return res.json(dados)
+    }catch(error){
+        res.status(500).json({ message:error })
     }
 }
 
