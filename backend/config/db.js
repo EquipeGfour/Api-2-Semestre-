1e7be1a5e59c8b132041cb2244f3d29905eb9402
Rodrigo Ribeiro <rodrigo.rsantos40@gmail.com>
2022-05-04 11:49:25 -0300

    atualizado heroku postgres

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,28 @@
 import { Sequelize } from "sequelize";
 import 'dotenv/config' 
 
-const db = new Sequelize(process.env.BD,'root',process.env.BD_SENHA,{
-    host:'localhost',
-    dialect:'mysql',
-    port:'3306'
-});
+const DB = process.env.BD
+const USER = 'root' 
+const senha = process.env.BD_SENHA
+const HOST = 'localhost'
+const PORT = 3306
+
+
+const url = process.env.DATABASE_URL || `mysql://${USER}:${senha}@${HOST}:${PORT}/${DB}`
+
+const db = new Sequelize(url, {
+    dialectOptions: {
+        charset: 'utf8mb4',
+        multipleStatements: true
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+            }
+            }
+            )
 
 export default db;
+
