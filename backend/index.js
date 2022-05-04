212254e07102800752b969458196073dc54f97fb
Rodrigo Ribeiro <rodrigo.rsantos40@gmail.com>
2022-05-04 12:21:44 -0300

    ajuste porta dinamica.

--- backend/index.js
+++ backend/index.js
@@ -49,5 +49,5 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 app.get ('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
-
-app.listen(5000 , ()=> console.log('Servidor rodando na porta 5000. :D'))
+const port = process.env.PORT || 5000;
+app.listen(port , ()=> console.log(`Servidor rodando na porta ${port}. :D`));
