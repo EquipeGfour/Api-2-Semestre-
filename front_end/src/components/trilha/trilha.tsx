eb1585dabee26bf007455521a5d0d2cfd4d28956
Rafael <rafael.waltrick@fatec.sp.gov.br>
2022-05-05 20:06:37 -0300

    Adicionado componente trilha.

---
+++ front_end/src/components/trilha/trilha.tsx
@@ -0,0 +1,26 @@
+import React,{Component} from "react";
+import {Link, Navigate, useNavigate} from 'react-router-dom';
+import './trilha.css';
+import { CriaHeader } from "../../functions";
+import axios from "axios";
+
+
+const Trilha:React.FC=()=>{
+
+
+    React.useEffect(()=>{
+        document.title = 'Trilha'
+    })
+
+
+    return(
+        <div>
+            
+        </div>
+    )
+
+}
+export default Trilha
+
+
+
