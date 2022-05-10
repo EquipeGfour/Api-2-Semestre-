a8351e941adab93471ff1dcf3ad8d383a5b3d162
Rodrigo Ribeiro <rodrigo.rsantos40@gmail.com>
2022-05-10 19:16:57 -0300

    Alterado plataforma de email.

--- backend/config/email.js
+++ backend/config/email.js
@@ -1,10 +1,14 @@
-import NodeMailer  from "nodemailer"
+import NodeMailer from "nodemailer"
 import "dotenv/config"
-console.log(process.env.EMAIL,process.env.EMAIL_SENHA)
-export const transporter = NodeMailer.createTransport({ 
-    service: 'gmail', 
-    auth: { 
-       user: process.env.EMAIL, 
-       pass: process.env.EMAIL_SENHA //colocar senha do email 
-     } 
-    });
+console.log(process.env.EMAIL, process.env.EMAIL_SENHA)
+export const transporter = NodeMailer.createTransport({
+  host: 'smtp.mail.ee',
+  port: 587,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS //colocar senha do email 
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
