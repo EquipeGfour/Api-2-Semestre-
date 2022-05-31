07939903162615aa68dcb5170550e4fe57404f8c
Rafael <rafael.waltrick@fatec.sp.gov.br>
2022-05-30 23:00:06 -0300

    Página assistir curso.

--- front_end/src/components/assistirCurso/assistirCurso.tsx
+++ front_end/src/components/assistirCurso/assistirCurso.tsx
@@ -11,20 +11,50 @@ import ReactTooltip from "react-tooltip";
 const AssistirCurso: React.FC = (props) => {
 
 
-    
 
-    React.useEffect(() => {            
+
+    React.useEffect(() => {
         document.title = 'Assitir-Curso'
     }, [])
 
-    return(
-        <div>
-        <video id="videoBanner" className="videoBanner" loop autoPlay muted controls  >
-            <source src='http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4' type='video/mp4'></source>            
-        </video>
+    return (
+        <div className="containerAssistir">
+            <h3 className="titulo-assistir">Nome do curso</h3>
+            <video id="videoBanner" className="videoBanner" controls  >
+                <source src='http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4' type='video/mp4'></source>
+            </video>
+            <h3 className="titulo-assistir">Nome do video</h3>
+            <hr className="linhaDivisao"></hr>
+            <h3 className="titulo-assistir">Arquivos da aula</h3>
+            <div className="arquivosAssistir">
+                <table className="responsive-table centered tabUp1">
+                    <thead>
+                        <tr className="linha">
+                            <th>Arquivo</th>
+                            <th></th>
+                            <th></th>
+                            <th>Baixar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Nome do arquivo</td>
+                            <td><span><button className="excluir">{/*<i className="material-icons delUp">clear</i>*/}</button></span></td>
+                            <td></td>
+                            <td>
+                                <div className="file-field input-field btn">
+                                    <i className="material-icons">file_download</i>
+                                </div>
+                            </td>
+
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
 
 
-    
+
 
         </div>
     )
