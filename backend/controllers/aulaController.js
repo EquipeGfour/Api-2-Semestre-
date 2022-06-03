554a6f37d960693af4bb2ff69758572866ed0a06
Nicolas Lima <nichollaslimma734@gmail.com>
2022-06-03 20:53:51 -0300

    ajustado endpoint de vincular colab.

--- backend/controllers/aulaController.js
+++ backend/controllers/aulaController.js
@@ -1,4 +1,5 @@
 import Aula from "../models/aula.js"
+import Colaborador from "../models/colaborador.js"
 
 export const criarAula = async (req, res) => {
     try{
@@ -43,3 +44,26 @@ export const listarAulaID = async (req, res) => {
         res.status(500).json({ message:error })
     }
 }
+
+export const verifyAulaEnd = async (req, res) => {
+    try{
+        const dados = await  Aula.findByPk(req.body.aula_id).then( (aula) => {
+            if(!aula) return { message:'Aula não encontrada '}
+            return Colaborador.findByPk(req.body.colab_id).then( async (colab) => {
+                try{
+                    const verify = await aula.hasColaborador(colab)
+                    if(verify) return { message:'Colaborador não vinculado' }
+                    await aula.addColaborador(colab, {through:{status:'Concluido'}})
+                    return aula
+                }catch(error){
+                    return { message:error }
+                }
+            })
+        })
+        res.json(dados)
+
+    }catch(error){
+        res.status(500).json({ message:error })
+    }
+}
+
--- backend/routes/aulaRouter.js
+++ backend/routes/aulaRouter.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { criarAula, listarAula, listarAulaID } from '../controllers/aulaController.js'
+import { criarAula, listarAula, listarAulaID, verifyAulaEnd } from '../controllers/aulaController.js'
 import { verifyAdm, verifyJWT } from '../controllers/loginController.js'
 
 const router = Router()
@@ -7,5 +7,6 @@ const router = Router()
 router.post('/criarAula/:id', verifyJWT, verifyAdm, criarAula)
 router.get('/listarAulas/:id', verifyJWT,listarAula)
 router.get('/aulaById/:id', verifyJWT,listarAulaID)
+router.put('/finalizarAula', verifyJWT, verifyAulaEnd)
 
 export default router
